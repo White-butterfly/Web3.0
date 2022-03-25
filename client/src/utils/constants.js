df2f0779845d3d0e0e125a6d83c3f21f035eaf88
muna <munaahmed212@yahoo.com>
2022-03-25 10:17:41 +0300

    crypto

---
+++ client/src/utils/constants.js
@@ -0,0 +1,15 @@
+
+import abi from './Transactions.json';
+
+
+export const contractAddress ="0xC39B48DFC77c937182989Cf75257968857e13654";
+
+export const contractABI = abi.abi;
+
+
+
+// export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+// export const contractABI = abi.abi;
+
+
+
